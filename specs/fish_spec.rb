e39e88a181c0d5aa939af808e34f77af90f39000
Darren Shankland <ds@4pq.co.uk>
2019-03-26 19:32:01 +0000

    adds fish class and test get name

--- specs/fish_spec.rb
+++ specs/fish_spec.rb
@@ -5,8 +5,11 @@ require_relative('../fish')
 class TestFish < MiniTest::Test
 
   def setup()
-
+    @fish = Fish.new('Carp')
   end
 
+  def test_get_fish_name()
+    assert_equal('Carp', @fish.name)
+  end
 
 end
