3d52ddf05e7b64b132f23bd1e108089b1576d7c7
Darren Shankland <ds@4pq.co.uk>
2019-03-26 19:52:25 +0000

    River now has lots of fish and we can add and remove fish

--- river.rb
+++ river.rb
@@ -7,5 +7,16 @@ class River
     @fishes = []
   end
 
+  def count_fish()
+    return @fishes.count()
+  end
+
+  def add_fish(fish)
+    @fishes.push(fish)
+  end
+
+  def remove_fish()
+    @fishes.pop()
+  end
 
 end
--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -1,15 +1,34 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../river')
+require_relative('../fish')
 
 class TestRiver < MiniTest::Test
 
   def setup()
     @river = River.new('Amazon')
+    @single_fish = Fish.new('Carp')
+    for i in 1..100 do
+      @river.add_fish(@single_fish)
+    end
   end
 
   def test_get_river_name()
     assert_equal('Amazon', @river.name)
+  end
+
+  def test_count_fish()
+    assert_equal(100, @river.count_fish())
+  end
+
+  def test_add_fish()
+    @river.add_fish(@single_fish)
+    assert_equal(101, @river.count_fish())
+  end
+
+  def test_remove_fish_from_river()
+    @river.remove_fish()
+    assert_equal(99, @river.count_fish())
   end
 
 end
